8bd380d2113503e1b5b5edfdabaf351763d95e84
Aditya Khosla <adikhosla@gmail.com>
2013-09-03 21:04:00 -0400

    updated imgread to allow images in RAM

--- util/imgread.m
+++ util/imgread.m
@@ -1,6 +1,13 @@
-function [I] = imgread(img, config)
-I = imread(img);
-scale = min(min(config.maxsize/size(I, 1), config.maxsize/size(I, 2)), 1);
+function [I] = imgread(img, c)
+
+if(ischar(img))
+    I = imread(img);
+else
+    I=img;
+end
+
+
+scale = min(min(c.maxsize/size(I, 1), c.maxsize/size(I, 2)), 1);
 if(scale~=1)
 	I = imresize(I, scale);
 end
