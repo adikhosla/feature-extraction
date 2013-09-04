e63594421904d9e882d2534a15843ce85ba4db46
Aditya Khosla <adikhosla@gmail.com>
2013-09-04 01:49:02 -0400

    fixed bugs in gist output filenames

--- features/gist/config_gist.m
+++ features/gist/config_gist.m
@@ -6,5 +6,5 @@ p.numberBlocks = 4;
 p.fc_prefilt = 4;
 p.feature_size = 512;
 
-p.train_file = '%s/train_gist_%d.mat';
-p.test_file = '%s/test_gist_%d.mat';
+p.train_file = ['%s/train_gist_' num2str(p.feature_size) '.mat'];
+p.test_file = ['%s/test_gist_' num2str(p.feature_size) '.mat'];
